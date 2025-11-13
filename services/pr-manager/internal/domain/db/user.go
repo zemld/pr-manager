c97e7266b9efa6f3d8baed2c33d3efdbac9eb1a7
zemld <zemlyanukhind@yandex.ru>
2025-11-13 17:51:41 +0300

    added user storager type for db

---
+++ services/pr-manager/internal/domain/db/user.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"github.com/zemld/pr-manager/pr-manager/internal/domain"
+)
+
+type UserStorage struct {
+	Config
+	Transactor
+	selectQuery string
+	updateQuery string
+	insertQuery string
+}
+
+func NewUserStorage(config Config, transactor Transactor) *UserStorage {
+	return &UserStorage{Config: config, Transactor: transactor}
+}
+
+func (s *UserStorage) SetSelectQuery(selectQuery string) {
+	s.selectQuery = selectQuery
+}
+
+func (s *UserStorage) SetUpdateQuery(updateQuery string) {
+	s.updateQuery = updateQuery
+}
+
+func (s *UserStorage) SetInsertQuery(insertQuery string) {
+	s.insertQuery = insertQuery
+}
+
+func (s *UserStorage) Select(userID string) (domain.User, error) {
+	rows, err := s.tx.Query(s.ctx, s.selectQuery, userID)
+	if err != nil {
+		return domain.User{}, err
+	}
+	defer rows.Close()
+
+	var user domain.User
+	if rows.Next() {
+		err = rows.Scan(&user.UserID, &user.Username, &user.TeamName, &user.IsActive)
+		if err != nil {
+			return domain.User{}, err
+		}
+	}
+	return user, nil
+}
+
+func (s *UserStorage) Update(user domain.User) error {
+	_, err := s.tx.Exec(s.ctx, s.updateQuery, user.IsActive, user.UserID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserStorage) Insert(user domain.User) error {
+	_, err := s.tx.Exec(s.ctx, s.insertQuery, user.UserID, user.Username, user.TeamName, user.IsActive)
+	if err != nil {
+		return err
+	}
+	return nil
+}
