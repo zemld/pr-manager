f73b7db77811e04bc63357d627f61cf3292f1084
zemld <zemlyanukhind@yandex.ru>
2025-11-13 17:38:06 +0300

    created interfaces to interact with storage

---
+++ services/pr-manager/internal/domain/storager/storager.go
@@ -0,0 +1,51 @@
+package storager
+
+import (
+	"github.com/zemld/pr-manager/pr-manager/internal/domain"
+)
+
+type Initializer interface {
+	Initialize() error
+}
+
+type Transactor interface {
+	Begin() error
+	Commit() error
+	Rollback() error
+}
+
+type Closer interface {
+	Close() error
+}
+
+type UserSelector interface {
+	Select(userID string) (domain.User, error)
+}
+
+type UserUpdater interface {
+	Update(user domain.User) error
+}
+
+type UserInserter interface {
+	Insert(user domain.User) error
+}
+
+type TeamSelector interface {
+	Select(teamName string) (domain.Team, error)
+}
+
+type TeamInserter interface {
+	Insert(team domain.Team) error
+}
+
+type PullRequestSelector interface {
+	Select(pullRequestID string) (domain.PullRequest, error)
+}
+
+type PullRequestUpdater interface {
+	Update(pullRequest domain.PullRequest) error
+}
+
+type PullRequestInserter interface {
+	Insert(pullRequest domain.PullRequest) error
+}
